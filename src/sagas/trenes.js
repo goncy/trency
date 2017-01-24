8a966397a5daf7fe524a843532802dbd1009fae2
goncy <goncy@live.com.ar>
2017-01-24 17:41:38 -0300

    Pasa mocks the calls a un archivo aparte

--- src/sagas/trenes.js
+++ src/sagas/trenes.js
@@ -2,10 +2,7 @@ import {put, call, takeEvery, select} from 'redux-saga/effects'
 
 import {fetchTrenes} from '../actions/api'
 import randomString from '../utils/randomString'
-
-const mockedResponse = () => [{'formacion_id': 1010071, 'latitud': '-34.699753', 'longitud': '-58.306417', 'ramal': 11, 'estado_servicio': 1, 'estado_mov': 1}, {'formacion_id': 1010066, 'latitud': '-34.753459', 'longitud': '-58.230769', 'ramal': 11, 'estado_servicio': 1, 'estado_mov': 1}, {'formacion_id': 200030, 'latitud': '-34.757164', 'longitud': '-58.223019', 'ramal': 12, 'estado_servicio': 1, 'estado_mov': 1}, {'formacion_id': 1010068, 'latitud': '-34.689736', 'longitud': '-58.327795', 'ramal': 12, 'estado_servicio': 1, 'estado_mov': 1}, {'formacion_id': 1010041, 'latitud': '-34.638829', 'longitud': '-58.380551', 'ramal': 12, 'estado_servicio': 1, 'estado_mov': 1}]
-
-const fetchError = fetchTrenes.failure('Hubo un error obteniendo la informacion de los trenes, prueba de nuevo mas tarde')
+import mockResponse from '../utils/mockResponse'
 
 export function* fetchTrenesApi () {
   const {preferences} = yield select()
@@ -18,9 +15,9 @@ export function* fetchTrenesApi () {
 
 export function* fetchTrenesSaga (): void {
   yield put(fetchTrenes.start())
-  const response = yield call(mockedResponse)
-  // const {response, error} = yield call(fetchTrenesApi)
-  // if (error) return yield put(fetchError)
+  const response = yield call(mockResponse, 'trenes')
+  /* const {response, error} = yield call(fetchTrenesApi)
+  if (error) return yield put(fetchTrenes.failure('Hubo un error obteniendo la informacion de los trenes, prueba de nuevo mas tarde')) */
   yield put(fetchTrenes.success(response))
 }
 
