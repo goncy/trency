02a14b65845b3e46a6bd8969843af670e26595df
goncy <goncy@live.com.ar>
2017-02-03 12:59:14 -0300

    Change editar text for volver atras button

--- src/components/Preferences/index.js
+++ src/components/Preferences/index.js
@@ -3,8 +3,6 @@ import {connect} from 'react-redux'
 
 import {clearPreferences} from '../../actions/preferences'
 
-import EditIcon from './edit.svg'
-
 import './Preferences.css'
 
 const Preferences = ({branch, line, station, clearPreferences}) => (
@@ -14,19 +12,10 @@ const Preferences = ({branch, line, station, clearPreferences}) => (
       <p>{`Ramal ${branch.name}`}</p>
       <p>{`En la estacion ${station.name}`}</p>
       <button
-        className='edit-btn'
+        className='button edit-btn'
         onClick={() => clearPreferences()}
       >
-        <span className='edit-text'>
-          EDITAR
-        </span>
-        <img
-          width='14px'
-          height='14px'
-          className='edit-img'
-          alt='edit'
-          src={EditIcon}
-        />
+        Volver atras
       </button>
     </div>
   </div>
