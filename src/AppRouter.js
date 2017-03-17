c57aa2cbf530f11d96ceb93b4446c7c59cbc11fd
goncy <goncy@live.com.ar>
2017-03-17 11:54:36 -0300

    Replace browserRouter with hashRouter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 
 import Line from './components/PreferencesSetter/scenes/Line'
 import Branch from './components/PreferencesSetter/scenes/Branch'
