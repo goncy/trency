63df457587b40a051584fe173161b4a6d5cd6a03
goncy <goncy@live.com.ar>
2017-03-26 12:48:53 -0300

    redeploy

--- src/constants.js
+++ src/constants.js
@@ -1562,4 +1562,4 @@ export const LINES = [
 export const SUCCESS_FETCH_TIME = 15000
 export const FAILURE_FETCH_TIME = 5000
 export const IDLE_TIME = 300000
-export const API_URL = 'http://localhost:8080/api/v1/data/'
+export const API_URL = 'https://trenesargentinosapi.herokuapp.com/api/v1/data/'
